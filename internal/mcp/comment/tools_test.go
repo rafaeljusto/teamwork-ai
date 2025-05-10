29efc03072bd1288906b806f556f572a5f78a963
Rafael Dantas Justo <rafael.justo@teamwork.com>
2025-05-10 09:34:50 -0400

    feat(mcp): add support for comments (#12)
    * feat(mcp): add support for comments
    
    Allow comments management in the Teamwork.com account via MCP server.
    
    * chore: add missing docs about new support for comments
    
    Update README of the MCP tool to mention support for comments.

---
+++ internal/mcp/comment/tools_test.go
@@ -0,0 +1,309 @@
+package comment_test
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
+	"github.com/rafaeljusto/teamwork-ai/internal/config"
+	"github.com/rafaeljusto/teamwork-ai/internal/mcp/comment"
+	"github.com/rafaeljusto/teamwork-ai/internal/teamwork"
+)
+
+func TestTools_retrieveComments(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-comments"
+	request.Params.Arguments = map[string]any{
+		"search-term": "test",
+		"user-ids":    []float64{1, 2, 3},
+		"page":        float64(1),
+		"page-size":   float64(10),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_retrieveFileComments(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-file-comments"
+	request.Params.Arguments = map[string]any{
+		"file-id":     float64(123),
+		"search-term": "test",
+		"user-ids":    []float64{1, 2, 3},
+		"page":        float64(1),
+		"page-size":   float64(10),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_retrieveMilestoneComments(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-milestone-comments"
+	request.Params.Arguments = map[string]any{
+		"milestone-id": float64(123),
+		"search-term":  "test",
+		"user-ids":     []float64{1, 2, 3},
+		"page":         float64(1),
+		"page-size":    float64(10),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_retrieveNotebookComments(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-notebook-comments"
+	request.Params.Arguments = map[string]any{
+		"notebook-id": float64(123),
+		"search-term": "test",
+		"user-ids":    []float64{1, 2, 3},
+		"page":        float64(1),
+		"page-size":   float64(10),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_retrieveTaskComments(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-task-comments"
+	request.Params.Arguments = map[string]any{
+		"task-id":     float64(123),
+		"search-term": "test",
+		"user-ids":    []float64{1, 2, 3},
+		"page":        float64(1),
+		"page-size":   float64(10),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_retrieveComment(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "retrieve-comment"
+	request.Params.Arguments = map[string]any{
+		"comment-id": float64(123),
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_createComment(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "create-comment"
+	request.Params.Arguments = map[string]any{
+		"object": map[string]any{
+			"type": "tasks",
+			"id":   float64(123),
+		},
+		"body":         "Example",
+		"content-type": "TEXT",
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+func TestTools_updateComment(t *testing.T) {
+	mcpServer := server.NewMCPServer("test-server", "1.0.0")
+	comment.Register(mcpServer, &config.Resources{
+		TeamworkEngine: engineMock{},
+	})
+
+	request := &toolRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      1,
+		CallToolRequest: mcp.CallToolRequest{
+			Request: mcp.Request{
+				Method: string(mcp.MethodToolsCall),
+			},
+		},
+	}
+	request.Params.Name = "update-comment"
+	request.Params.Arguments = map[string]any{
+		"comment-id":   float64(123),
+		"body":         "Example",
+		"content-type": "TEXT",
+	}
+
+	encodedRequest, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("failed to encode request: %v", err)
+	}
+
+	ctx := context.Background()
+	message := mcpServer.HandleMessage(ctx, encodedRequest)
+	if err, ok := message.(mcp.JSONRPCError); ok {
+		t.Errorf("tool failed to execute: %v", err.Error)
+	}
+}
+
+type toolRequest struct {
+	mcp.CallToolRequest
+
+	JSONRPC string `json:"jsonrpc"`
+	ID      int64  `json:"id"`
+}
+
+type engineMock struct {
+}
+
+func (e engineMock) Do(context.Context, teamwork.Entity, ...teamwork.Option) error {
+	return nil
+}
